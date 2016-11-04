05038db6e9329c108e1caad96e563d6792c2f381
dvwzj <iguphai_@msn.com>
2016-11-04 18:16:15 +0700

    remove _k from url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router, browserHistory, hashHistory} from 'react-router';
+import {Router, browserHistory, useRouterHistory} from 'react-router';
+import {createHashHistory} from 'history';
 import routes from './routes';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
@@ -22,7 +23,8 @@ if(username && password) {
 
 // Create an enhanced history that syncs navigation events with the store
 // const history = syncHistoryWithStore(browserHistory, store);
-const history = syncHistoryWithStore(hashHistory, store); // Hash History, fix
+const appHashHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+const history = syncHistoryWithStore(appHashHistory, store); // Hash History, fix
 
 render(
     <Provider store={store}>
