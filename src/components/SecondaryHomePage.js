7dd6d65f867500627896d9a42dc6707d9296f992
dvwzj <iguphai_@msn.com>
2016-11-17 19:28:57 +0700

    added home images for secondary project

--- src/components/SecondaryHomePage.js
+++ src/components/SecondaryHomePage.js
@@ -2,8 +2,9 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
 import {bindActionCreators} from 'redux';
-import {Grid, Row, Col} from 'react-flexbox-grid';
-import {PageHeading, Panel} from 'react-semi-theme/widgets';
+//import {Grid, Row, Col} from 'react-flexbox-grid';
+import {PageHeading, Panel, SemiHeader} from 'react-semi-theme/widgets';
+import {SemiGrid, Row, Col} from 'react-semi-theme/grid';
 import Divider from 'material-ui/Divider';
 import RaisedButton from 'material-ui/RaisedButton';
 
@@ -24,84 +25,107 @@ class HomePage extends Component {
 	};
 
 	render() {
-		let example = require('../assets/img/upload-example.png');
+		//let example = require('../assets/img/upload-example.png');
+		let icons = Array.from(Array(13), (v, k)=>require(`../assets/img/icon/icon-${('00'+(k+1)).slice(-2)}.png`));
+		let rowStyle = {marginBottom: 24};
+		let anchorStyle = {textDecoration: 'none'};
+		let divStyle = {border: '1px solid #ddd', textAlign: 'center', paddingBottom: 16};
+		let headerStyle = {margin: 0, fontSize: 16, height: 32};
 		return (
 			<div>
-				<PageHeading title="Home" description="description" />
-				<Grid fluid className="content-wrap">
-					<Row>
-						<Col xs lg={10} lgOffset={1}>
+				<SemiGrid>
+					<Row center="xs" style={{overflow: 'hidden'}}>
+						<Col xs="96%" xl="90%" style={{maxWidth: 1400}}>
+							<div className="mtpc-logo"></div>
 							<Panel title="Home">
 								<div className="con-pad">
-									<Grid>
-										<Row>
-											<Col xs md={4}>
-												<a href="#/hair"><h3>{this.context.translate('menu:hair')}</h3></a>
-												<a href="#/hair"><img src={example} width="100%" /></a>
+									<SemiGrid>
+										<Row style={rowStyle}>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/hair" style={anchorStyle}><img src={icons[0]} width="100%" /></a>
+													<a href="#/hair" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:hair')}</SemiHeader></a>
+												</div>
 											</Col>
-											<Col xs md={4}>
-												<a href="#/forehead"><h3>{this.context.translate('menu:forehead')}</h3></a>
-												<a href="#/forehead"><img src={example} width="100%" /></a>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/forehead" style={anchorStyle}><img src={icons[1]} width="100%" /></a>
+													<a href="#/forehead" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:forehead')}</SemiHeader></a>
+												</div>
 											</Col>
-											<Col xs md={4}>
-												<a href="#/nose"><h3>{this.context.translate('menu:nose')}</h3></a>
-												<a href="#/nose"><img src={example} width="100%" /></a>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/nose" style={anchorStyle}><img src={icons[2]} width="100%" /></a>
+													<a href="#/nose" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:nose')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/nose-recon" style={anchorStyle}><img src={icons[3]} width="100%" /></a>
+													<a href="#/nose-recon" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:nose-recon')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/eyelids" style={anchorStyle}><img src={icons[4]} width="100%" /></a>
+													<a href="#/eyelids" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:eyelids')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/around-eyes" style={anchorStyle}><img src={icons[5]} width="100%" /></a>
+													<a href="#/around-eyes" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:around-eyes')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/eyebrow" style={anchorStyle}><img src={icons[6]} width="100%" /></a>
+													<a href="#/eyebrow" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:eyebrow')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/lips" style={anchorStyle}><img src={icons[7]} width="100%" /></a>
+													<a href="#/lips" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:lips')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/breast" style={anchorStyle}><img src={icons[8]} width="100%" /></a>
+													<a href="#/breast" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:breast')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/chin" style={anchorStyle}><img src={icons[9]} width="100%" /></a>
+													<a href="#/chin" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:chin')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/cheek-bulge" style={anchorStyle}><img src={icons[10]} width="100%" /></a>
+													<a href="#/cheek-bulge" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:cheek-bulge')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/liposuction" style={anchorStyle}><img src={icons[11]} width="100%" /></a>
+													<a href="#/liposuction" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:liposuction')}</SemiHeader></a>
+												</div>
+											</Col>
+											<Col xs="20%">
+												<div style={divStyle}>
+													<a href="#/implants" style={anchorStyle}><img src={icons[12]} width="100%" /></a>
+													<a href="#/implants" style={anchorStyle}><SemiHeader style={headerStyle}>{this.context.translate('menu:implants')}</SemiHeader></a>
+												</div>
 											</Col>
 										</Row>
-										<Row>
-											<Col xs md={4}>
-												<a href="#/nose-recon"><h3>{this.context.translate('menu:nose-recon')}</h3></a>
-												<a href="#/nose-recon"><img src={example} width="100%" /></a>
-											</Col>
-											<Col xs md={4}>
-												<a href="#/eyelids"><h3>{this.context.translate('menu:eyelids')}</h3></a>
-												<a href="#/eyelids"><img src={example} width="100%" /></a>
-											</Col>
-											<Col xs md={4}>
-												<a href="#/around-eyes"><h3>{this.context.translate('menu:around-eyes')}</h3></a>
-												<a href="#/around-eyes"><img src={example} width="100%" /></a>
-											</Col>
-										</Row>
-										<Row>
-											<Col xs md={4}>
-												<a href="#/eyebrow"><h3>{this.context.translate('menu:eyebrow')}</h3></a>
-												<a href="#/eyebrow"><img src={example} width="100%" /></a>
-											</Col>
-											<Col xs md={4}>
-												<a href="#/lips"><h3>{this.context.translate('menu:lips')}</h3></a>
-												<a href="#/lips"><img src={example} width="100%" /></a>
-											</Col>
-											<Col xs md={4}>
-												<a href="#/breast"><h3>{this.context.translate('menu:breast')}</h3></a>
-												<a href="#/breast"><img src={example} width="100%" /></a>
-											</Col>
-										</Row>
-										<Row>
-											<Col xs md={4}>
-												<a href="#/chin"><h3>{this.context.translate('menu:chin')}</h3></a>
-												<a href="#/chin"><img src={example} width="100%" /></a>
-											</Col>
-											<Col xs md={4}>
-												<a href="#/cheek-bulge"><h3>{this.context.translate('menu:cheek-bulge')}</h3></a>
-												<a href="#/cheek-bulge"><img src={example} width="100%" /></a>
-											</Col>
-											<Col xs md={4}>
-												<a href="#/liposuction"><h3>{this.context.translate('menu:liposuction')}</h3></a>
-												<a href="#/liposuction"><img src={example} width="100%" /></a>
-											</Col>
-										</Row>
-										<Row>
-											<Col xs md={4}>
-												<a href="#/implants"><h3>{this.context.translate('menu:implants')}</h3></a>
-												<a href="#/implants"><img src={example} width="100%" /></a>
-											</Col>
-										</Row>
-									</Grid>
+									</SemiGrid>
 								</div>
 							</Panel>
 						</Col>
 					</Row>
-				</Grid>
+				</SemiGrid>
 			</div>
 		);
 	}
