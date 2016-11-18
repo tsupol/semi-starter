5286570692bafb8b6842ef1cc1e348045bd20318
dvwzj <iguphai_@msn.com>
2016-11-18 18:06:12 +0700

    fix switch languages button

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {Router, browserHistory, useRouterHistory} from 'react-router';
 import {createHashHistory} from 'history';
-import routes from './routes/primary.js';
+import routes from './routes/secondary.js';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
